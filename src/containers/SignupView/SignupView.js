5ab1526df624e6be52adf13d1f867b85c03219b3
Isuru Bandaranayake <isbana.git@gmail.com>
2020-05-26 21:37:45 +0530

    Added folder structure

---
+++ src/containers/SignupView/SignupView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SignupView extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignupView
