4b696e5ce659bf44c70b90b2a25dc654d603beff
Isuru Bandaranayake <isbana.git@gmail.com>
2020-06-16 16:42:59 +0530

    Added App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavRouter from '../src/navigation/NavRouter'
 
+import './App.css'
+
 function App() {
   return (
     <div className="App">
-      <NavRouter/>
+      <NavRouter />
     </div>
   );
 }
