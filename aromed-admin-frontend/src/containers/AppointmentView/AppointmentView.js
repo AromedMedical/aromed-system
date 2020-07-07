5ad7b1fd13f4d1328c179f496a73843f4a889e4a
Isuru Bandaranayake <isbana.git@gmail.com>
2020-07-08 00:41:14 +0530

    Add directory structure

---
+++ aromed-admin-frontend/src/containers/AppointmentView/AppointmentView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AppointmentView extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AppointmentView
