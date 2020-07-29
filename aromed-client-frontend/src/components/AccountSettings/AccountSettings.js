9a113bc32c34baf3a527c371b43b50abaa862f62
Prabani Rathnasiri <hhprabanirathnasiri@gmail.com>
2020-07-28 23:20:21 -0700

    Change Accountsettings GROUP2-86

--- aromed-client-frontend/src/components/AccountSettings/AccountSettings.js
+++ aromed-client-frontend/src/components/AccountSettings/AccountSettings.js
@@ -1,9 +1,64 @@
 
 import React, { Component } from 'react'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
+import { Link, withRouter } from 'react-router-dom'
+import { compose } from 'recompose';
 
-export class AccountSettings extends Component {
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+
+const INITIAL_STATE = {
+    error: null,
+    currpassword: '',
+    newpassword: '',
+    renewpassword: '',
+};
+
+class AccountSettings extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value.trim()
+        });
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        this.setState({ error: '' });
+        const { currpassword, newpassword, renewpassword } = this.state;
+
+        if (newpassword !== renewpassword) {
+            this.setState({ error: 'Passwords does not match' });
+        }
+        else {
+            this.props.firebase
+                .doCreateUserWithEmailAndPassword(email, newpassword)
+                .then(authUser => {
+                    this.setState({ ...INITIAL_STATE });
+                    this.props.history.push(ROUTES.LOGIN);
+                })
+                .catch(error => {
+                    this.setState({ error: error.message });
+                });
+        }
+    }
+
     render() {
+        const {
+            error,
+            currpassword,
+            newpassword,
+            renewpassword,
+        } = this.state;
+
+
         return (
             <div className="container col-lg-5 my-3 min-vh-100 d-flex flex-column justify-content-center">
                 <Form className="bg-light p-5">
@@ -33,5 +88,6 @@ export class AccountSettings extends Component {
         )
     }
 }
+const AccountSettingsView = compose(withRouter, withFirebase)(AccountSettingsBase);
 
 export default AccountSettings;
