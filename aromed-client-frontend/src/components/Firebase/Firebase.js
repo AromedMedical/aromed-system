1c5ebd5212739992eed4e90e777cc00d0f1d20cb
Isuru Bandaranayake <isbana.git@gmail.com>
2020-07-30 07:46:14 +0530

    Removed a modified file from pull request

--- aromed-client-frontend/src/components/Firebase/Firebase.js
+++ aromed-client-frontend/src/components/Firebase/Firebase.js
@@ -1,15 +1,11 @@
 import app from 'firebase/app';
 import 'firebase/auth';
-import firebase from 'firebase';
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL
 };
-firebase.initializeApp(config);
-export const auth = firebase.auth;
-export const db = firebase.database();
 
 class Firebase {
     constructor() {
