52a7c4bc71a42d5602041c0c96d0ed52c93cc0aa
Isuru Bandaranayake <isbana.git@gmail.com>
2020-08-06 15:11:13 +0530

    Fixed Alert box

--- aromed-client-frontend/src/index.js
+++ aromed-client-frontend/src/index.js
@@ -8,31 +8,20 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 
-
-// optional configuration
 const options = {
-  // you can also just use 'bottom center'
   position: positions.BOTTOM_CENTER,
   timeout: 5000,
   offset: '30px',
-  // you can also just use 'scale'
   transition: transitions.SCALE
 }
-
-const Root = () => (
-  <AlertProvider template={AlertTemplate} {...options}>
-    <App />
-  </AlertProvider>
-)
-
-//render(<Root />, document.getElementById('root'))
 
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseContext.Provider value={new Firebase()}>
-      <App />
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
     </FirebaseContext.Provider>
   </React.StrictMode>,
-  <Root />,
   document.getElementById('root')
 );
